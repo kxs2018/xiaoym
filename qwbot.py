16e1180bbe0f18dc5a968d3fcfc6296a70a2d010
kxs2018 <82091384+kxs2018@users.noreply.github.com>
2023-09-17 12:11:42 +0800

    Update qwbot.py

--- qwbot.py
+++ qwbot.py
@@ -7,17 +7,22 @@ import os
 import random
 import requests
 import config
-
+import datetime
 qwbotkey = config.qwbotkey
-imgurl = f''  # f'https://xxxx.com/{random.randint(1, 170)}.jpg' 可选项，每次通知随机图片
 
 
-def qwbot(digest, title=None, url=None):
-    if not title and not url:
+
+def format_time():
+    t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return t
+
+
+def send(msg, title='通知', url=None):
+    if not url:
         data = {
             "msgtype": "text",
             "text": {
-                "content": digest,
+                "content": f"{title}\n\n{msg}\n\n本通知by：惜之酱\n仓库地址：https://github.com/kxs2018/yuedu\ntg频道：https://t.me/+uyR92pduL3RiNzc1\n通知时间：{format_time()}",
                 # "mentioned_list": ["@all"],
             }
         }
@@ -28,23 +33,19 @@ def qwbot(digest, title=None, url=None):
                 "articles": [
                     {
                         "title": title,
-                        "description": digest,
+                        "description": msg,
                         "url": url,
-                        "picurl": imgurl
+                        "picurl": 'https://i.ibb.co/7b0WtQH/17-32-15-2a67df71228c73f35ca47cabaa826f17-eb5ce7b1e.png'
                     }
                 ]
             }
         }
     whurl = f'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={qwbotkey}'
-    resp = requests.post(whurl, data=json.dumps(data))
-    resp.raise_for_status()
-    return resp.json()
-
-
-def send(digest, title=None, url=None):
-    if qwbotkey:
-        qw = qwbot(digest, title, url=url)
-        return qw
+    resp = requests.post(whurl, data=json.dumps(data)).json()
+    if resp.get('errcode') != 0:
+        print('消息发送失败，请检查key和发送格式')
+        return False
+    return resp
 
 
 if __name__ == '__main__':
