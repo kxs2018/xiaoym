89d8acf4db809b6543b56d9e3c30c192b680536d
kxs2018 <82091384+kxs2018@users.noreply.github.com>
2023-09-18 15:12:00 +0800

    Create k花花阅读多线程单文件版.py

---
+++ k花花阅读多线程单文件版.py
@@ -0,0 +1,300 @@
+"""
+花花阅读入口：http://mr138301519.nmyebvvmntj.cloud/user/index.html?mid=EG5EVNLF3
+
+http://u.cocozx.cn/api/user/info
+抓包 info接口的请求体中的un和token参数
+
+注意：脚本变量使用的单引号、双引号、逗号都是英文状态的
+注意：脚本变量使用的单引号、双引号、逗号都是英文状态的
+注意：脚本变量使用的单引号、双引号、逗号都是英文状态的
+------------------------------------------------------
+青龙配置方式： 2选1
+1.青龙添加环境变量名称 ：aiock
+青龙添加环境变量参数
+单账户 [{"un": "xxxx", "token": "xxxxx","name":"彦祖"}]
+多账户[{"un": "xxxx", "token": "xxxxx","name":"彦祖"},{"un": "xxxx", "token": "xxxxx","name":"德华"},{"un": "xxxx", "token": "xxxxx"}]
+
+提现标准默认是5000
+内置推送企业微信群机器人
+参考 https://github.com/kxs2018/yuedu/blob/main/获取企业微信群机器人key.md 获取key，并关注插件！！！
+青龙添加环境变量名称 ：qwbotkey
+青龙添加环境变量参数 ："abcdefg"
+2.青龙配置文件
+export aiock=[{"un": "xxxx", "token": "xxxxx","name":"彦祖"}]
+如报错，可尝试 export aiock='''[{"un": "xxxx", "token": "xxxxx","name":"彦祖"}]'''
+export qwbotkey="abcdefg"
+------------------------------------------------------
+达到标准自动提现
+"""
+import json
+from random import randint
+import os
+import time
+import requests
+import ast
+import re
+from lxml import etree
+import datetime
+import threading
+from queue import Queue
+
+"""debug模式开关"""
+debug = 0
+"""1为开，打印调试日志；0为关，不打印"""
+
+"""线程数量设置"""
+max_workers = 3
+"""设置为3，即最多有3个任务同时进行"""
+
+qwbotkey = 'a731d446-9f59-498c-a345-32217ef6f29e'
+# qwbotkey = os.getenv('qwbotkey')
+# aiock = os.getenv('aiock')
+aiock = '''[
+        {"un": "oC34q66vpqmGFO2w17SifydgflDI", "token": "7c610752e26e91498da5035ade25af95", "name": "kang"},
+        # {"un":"oC34q6zcFSfF_hq8oIQ7eIpKp1d0","token":"c3995192cf7f3005861633ca05bbf2fb","name":"雨嫣"},
+        {"un": "oC34q61aushoJ-8mMtqFlxThb5sk", "token": "fa3c4894f081b23d6d598a6d63ca4312", "name": "皮蛋"},
+        {"un": "oC34q61PDQ8YzPLKd069EgsBJYjI", "token": "085b8b9f602c097187932464082bdfeb", "name": "AI","uid":"UID_9MZNoxoOvabi8ug30Grf66TZ266g"},
+        {"un": "oC34q61t3Fn11irLNE08QzUJ-JiM", "token": "26e28e9d923ef2499dac10dba7e95c03", "name": "惜之酱","uid":"UID_T4xlqWx9xPGMCPgUH0Zot1srnagh"},
+        # {"un": "oC34q633o6-4dfRhfCgPT2QMNcjU", "token": "5370c55256bdd296108d3911465a027c", "name": "木婉清"},
+        # {"un": "oC34q6zFjesQRO57vpI6Mq9Zvm9Q", "token": "3caf1ee6500c94d3814fdb47fd1b171c", "name": "晓旭",
+        #  "uid": "UID_b8ZUBZq3baZqrLY9dsy4kTGq1j2n"},
+        # {"un": "oC34q60ZQ_xDiNFwQ5EJNdBc6dX8", "token": "d9ec882fae3f45771239c0b3aa9bfe99", "name": "666"},
+    ]'''
+
+if not qwbotkey or not aiock:
+    print('凡人，你还没准备好')
+    exit()
+
+
+def ftime():
+    t = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return t
+
+
+def debugger(text):
+    if debug:
+        print(text)
+
+
+def send(msg, title='通知', url=None):
+    if not title or not url:
+        data = {
+            "msgtype": "text",
+            "text": {
+                "content": f"{title}\n\n{msg}\n\n本通知by：https://github.com/kxs2018/yuedu\ntg频道：https://t.me/+uyR92pduL3RiNzc1\n通知时间：{ftime()}",
+                # "mentioned_list": ["@all"],
+            }
+        }
+    else:
+        data = {
+            "msgtype": "news",
+            "news": {
+                "articles": [
+                    {
+                        "title": title,
+                        "description": msg,
+                        "url": url,
+                        "picurl": 'https://i.ibb.co/7b0WtQH/17-32-15-2a67df71228c73f35ca47cabaa826f17-eb5ce7b1e.png'
+                    }
+                ]
+            }
+        }
+    whurl = f'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key={qwbotkey}'
+    resp = requests.post(whurl, data=json.dumps(data)).json()
+    if resp.get('errcode') != 0:
+        print('消息发送失败，请检查key和发送格式')
+        return False
+    return resp
+
+
+def getmpinfo(link):
+    if not link or link == '':
+        return False
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Linux; Android 13; ANY-AN00 Build/HONORANY-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/111.0.5563.116 Mobile Safari/537.36 XWEB/5235 MMWEBSDK/20230701 MMWEBID/2833 MicroMessenger/8.0.40.2420(0x28002855) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64'}
+    res = requests.get(link, headers=headers)
+    html = etree.HTML(res.text)
+    # print(res.text)
+    title = html.xpath('//meta[@*="og:title"]/@content')
+    if title:
+        title = title[0]
+    url = html.xpath('//meta[@*="og:url"]/@content')
+    if url:
+        url = url[0].encode().decode()
+    biz = re.findall(r'biz=(.*?)&', link) or re.findall(r'biz=(.*?)&', url)
+    if biz:
+        biz = biz[0]
+    username = html.xpath('//div[@class="wx_follow_nickname"]/text()|//strong[@role="link"]/text()|//*[@href]/text()')
+    if username:
+        username = username[0].strip()
+    id = re.findall(r"user_name.DATA'\) : '(.*?)'", res.text) or html.xpath(
+        '//span[@class="profile_meta_value"]/text()')
+    if id:
+        id = id[0]
+    ctt = re.findall(r'createTime = \'(.*)\'', res.text)
+    if ctt:
+        ctt = ctt[0][5:]
+    text = f'{ctt}|{title}|{biz}|{username}|{id}'
+    mpinfo = {'biz': biz, 'text': text}
+    return mpinfo
+
+
+class Allinone:
+    def __init__(self, ck):
+        self.name = ck['name']
+        self.s = requests.session()
+        self.payload = {"un": ck['un'], "token": ck['token'], "pageSize": 20}
+        self.s.headers = {'Accept': 'application/json, text/javascript, */*; q=0.01',
+                          'Content-Type': 'application/json; charset=UTF-8',
+                          'Host': 'u.cocozx.cn',
+                          'Connection': 'keep-alive',
+                          'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 NetType/WIFI MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x6309070f) XWEB/8391 Flue",
+                          'Origin': 'http://mr1694971896247.dswxin.cn', }
+        self.headers = self.s.headers.copy()
+        self.msg = ''
+
+    def get_readhost(self):
+        url = "http://u.cocozx.cn/api/user/getReadHost"
+        res = self.s.post(url, json=self.payload).json()
+        debugger(f'readhome {res}')
+        self.readhost = res.get('result')['host']
+        self.headers['Origin'] = self.readhost
+        self.msg += f'邀请链接：{self.readhost}/user/index.html?mid={self.huid}\n'
+
+    def stataccess(self):
+        url = 'http://u.cocozx.cn/api/user/statAccess'
+        self.s.post(url, json=self.payload).json()
+
+    def get_info(self):
+        try:
+            response = self.s.post("http://u.cocozx.cn/api/user/info", json=self.payload).json()
+            result = response.get("result")
+            debugger(f'get_info {response}')
+            us = result.get('us')
+            if us == 2:
+                self.msg += f'账号：{self.name}已被封\n'
+                return False
+            self.msg += f"""账号:{self.name}，今日阅读次数:{result["dayCount"]}，当前元宝:{result["moneyCurrent"]}，累计阅读次数:{result["doneWx"]}\n"""
+            money = int(result["moneyCurrent"])
+            self.huid = result.get('uid')
+            return money
+        except:
+            return False
+
+    def psmoneyc(self):
+        data = {**self.payload, **{'mid': self.huid}}
+        try:
+            response = self.s.post("http://u.cocozx.cn/api/user/psmoneyc", json=data).json()
+            self.msg += f"感谢下级送来的{response['result']['val']}花儿\n"
+        except:
+            pass
+        return
+
+    def get_status(self):
+        res = requests.post("http://u.cocozx.cn/api/user/read", headers=self.headers, json=self.payload).json()
+        debugger(f'getstatus {res}')
+        self.status = res.get("result").get("status")
+        if self.status == 40:
+            self.msg += "文章还没有准备好\n"
+            return
+        elif self.status == 50:
+            self.msg += "阅读失效\n"
+            return
+        elif self.status == 60:
+            self.msg += "已经全部阅读完了\n"
+            return
+        elif self.status == 70:
+            self.msg += "下一轮还未开启\n"
+            return
+        elif self.status == 10:
+            taskurl = res["result"]["url"]
+            self.msg += '-' * 50 + "\n阅读链接获取成功\n"
+            return taskurl
+
+    def submit(self):
+        data = {**{'type': 1}, **self.payload}
+        response = requests.post("http://u.cocozx.cn/api/user/submit?zx=&xz=1", headers=self.headers, json=data)
+        result = response.json().get('result')
+        debugger('submit ' + response.text)
+        self.msg += f'阅读成功,获得元宝{result["val"]}，当前剩余次数:{result["progress"]}\n'
+
+    def read(self):
+        while True:
+            taskurl = self.get_status()
+            if not taskurl:
+                if self.status == 30:
+                    time.sleep(3)
+                    continue
+                break
+            mpinfo = getmpinfo(taskurl)
+            self.msg += '开始阅读 ' + mpinfo['text'] + '\n'
+            t = randint(7, 10)
+            if mpinfo['biz'] == "Mzg2Mzk3Mjk5NQ==":
+                self.msg += '当前正在阅读检测文章\n'
+                send(mpinfo['text'], f'{self.name}  花花阅读正在读检测文章', taskurl)
+                time.sleep(50)
+            # print(f'模拟阅读{t}秒')
+            time.sleep(t)
+            self.submit()
+
+    def tixian(self):
+        money = self.get_info()
+        if 10000 <= money < 49999:
+            txe = 10000
+        elif 5000 <= money < 10000:
+            txe = 5000
+        # elif 3000 <= money < 5000:
+        #     txe = 3000
+        elif money >= 50000:
+            txe = 50000
+        else:
+            self.msg += '你的花儿不多了\n'
+            return False
+        self.msg += f"提现金额:{txe}"
+        data = {**self.payload, **{"val": txe}}
+        try:
+            res = self.s.post("http://u.cocozx.cn/api/user/wd", json=data).json()
+            self.msg += f"提现结果:{res.get('msg')}\n"
+        except:
+            self.msg += f"自动提现不成功，发送通知手动提现\n"
+            send(f'可提现金额 {int(txe) / 10000}元，点击提现', title=f'惜之酱提醒您 {self.name} 花花阅读可以提现了',
+                 url=f'{self.readhost}/user/index.html?mid=FK73K93DA')
+
+    def run(self):
+        self.msg += "*" * 50 + '\n'
+        if self.get_info():
+            self.stataccess()
+            self.get_readhost()
+            self.psmoneyc()
+            self.read()
+            self.tixian()
+        print(self.msg.strip())
+
+
+def yd(q):
+    while not q.empty():
+        ck = q.get()
+        try:
+            api = Allinone(ck)
+            api.run()
+        except Exception as e:
+            print(e)
+
+
+if __name__ == '__main__':
+    try:
+        aiock = ast.literal_eval(aiock)
+    except:
+        pass
+    q = Queue()
+    threads = []
+    for i in aiock:
+        q.put(i)
+    for i in range(max_workers):
+        t = threading.Thread(target=yd, args=(q,))
+        t.start()
+        threads.append(t)
+        time.sleep(60)  # 每隔60秒，加入一个账号开始阅读
+    for thread in threads:
+        thread.join()
+    print('*' * 50)
