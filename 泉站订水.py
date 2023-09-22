ffca5374f8c2ae70f2dae671ffcab5b6ce009afe
kxs2018 <kxs2018@aliyun.com>
2023-09-22 17:15:12 +0800

    默认更改列表

--- 泉站订水.py
+++ 泉站订水.py
@@ -31,7 +31,7 @@ def send(msg, title='通知', url=None):
         data = {
             "msgtype": "text",
             "text": {
-                "content": f"{title}\n\n{msg}\n\n本通知by：https://github.com/kxs2018/xiaoym\n[点击加入惜之酱tg频道](https://t.me/+uyR92pduL3RiNzc1)\n通知时间：{ftime()}",
+                "content": f"{title}\n\n{msg}\n\n本通知by：https://github.com/kxs2018/xiaoym\n惜之酱tg频道:https://t.me/+uyR92pduL3RiNzc1\n通知时间：{ftime()}",
                 # "mentioned_list": ["@all"],
             }
         }
