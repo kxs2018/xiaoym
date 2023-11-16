109c8a29ee9f366e3579cc4302adee4697e87cbf
kxs2018 <kxs2018@aliyun.com>
2023-11-16 14:21:24 +0800

    up

--- k_config.py
+++ k_config.py
@@ -17,7 +17,24 @@ pushconfig = {"appToken":"","topicids":[]}
 
 """元宝智慧星空内部互助设置"""
 invite_info = {'name':'','code':''}  # 仓库脚本无需理会，收费项目专用
+"""鱼儿设置"""
+yu_config = {
+        'max_workers': 5,  # 线程数量设置 设置为5，即最多有5个任务同时进行
 
+        'txbz': 0.5,  # 设置提现标准 单位：元
+
+        'sendable': 1,  # 企业微信推送开关 1开0关
+
+        'pushable': 1,  # wxpusher推送开关 1开0关
+
+        'delay_time': 20,  # 并发延迟设置 设置为20即每隔20秒新增一个号做任务，直到数量达到max_workers
+
+        'blacklist':[], # 提现黑名单设置,黑名单中的账号不自动提现，填入ck中的name,['name1','name2']。
+
+        'yuck':[], # ck设置，优先从环境变量中获取，[{'name':'德华','ck':'抓包的ck值'},{'name':'彦祖','ck':'抓包的ck值','uid':'UID_xxx'}]name值随意，方便自己辨认即可。uid是wxpusher一对一通知专属设置，其它情况不要填
+
+    }
+"""鱼儿设置完毕"""
 
 """可乐设置"""
 kl_config={
