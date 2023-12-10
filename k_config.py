08a2446d61fb4a6ddd939a257f977c74c5060989
kxs2018 <kxs2018@aliyun.com>
2023-12-10 16:24:23 +0800

    up

--- k_config.py
+++ k_config.py
@@ -15,8 +15,25 @@ qwbotkey=''  # 脚本优先从环境变量获取，没有获取到再从此处�
 pushconfig = {"appToken":"","topicids":[]} 
 """脚本优先从环境变量获取，没有获取到再从此处获取，"topicids":["123456"],一对一推送无需设置toppicids。已实装脚本：钢镚、每天赚"""
 
-"""元宝智慧星空内部互助设置"""
-invite_info = {'name':'','code':''}  # 仓库脚本无需理会，收费项目专用
+"""傻狗设置"""
+sg_config={
+    'max_workers': 3,  # 线程数量设置,设置为5，即最多有5个任务同时进行
+
+    'txbz': 0.3,  # 设置提现标准,0.3 5 10 20 共四档，单位元
+
+    'sendable': 1,  # 企业微信推送开关,1为开，0为关开启后必须设置qwbotkey才能运行
+
+    'pushable': 0,  # wxpusher推送开关,1为开，0为关,开启后必须设置pushconfig才能运行
+
+    'delay_time': 30,  # 并发延迟设置,设置为30即每隔30秒新增一个号做任务，直到数量达到max_workers
+
+    'blacklist':[], # 提现黑名单设置,黑名单中的账号不自动提现，填入ck中的name,['name1','name2']。
+    'shuffle':0,
+    'sgck':[
+    ], # ck设置，优先从环境变量中获取，[{'name':'德华','sid':'xxxxx'},{'name':'彦祖','sid':'xxxxx','uid':'xx'}]name值随意，方便自己辨认即可。uid是wxpusher一对一通知专属设置，其它情况不要填
+}
+"""傻狗设置完毕"""
+
 """鱼儿设置"""
 yu_config = {
         'max_workers': 5,  # 线程数量设置 设置为5，即最多有5个任务同时进行
