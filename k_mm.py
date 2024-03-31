437c5c7e857f63e78889736e0241d205a5788937
xizhiai <kafeidou@aliyun.com>
2024-03-31 13:31:43 +0800

    up

--- k_mm.py
+++ k_mm.py
@@ -1,51 +1,48 @@
+# -*- coding: utf-8 -*-
+
 """
-new Env('猫猫阅读');
-cron: 6 6 6 6 6
-先运行脚本，有问题到群里问 http://t.me/xiaoymgroup
-"""
-"""
-关于config.py本脚本必须的设置项：
-1.qwbotkey 
-2.pushconfig  
-以上二选1
-3.mmck 格式：[{'name':'xxx','ck':'bbus=xxx'},{'name':'xxx','uid':'UID_xxxxx','ck':'xxx','zfb_aaount':'11123455','zfb_name':'刘德华'}]
-name值随意，方便自己辨认即可。ck是抓包数据。uid是wxpusher一对一通知专属设置，其它情况不要填，支付宝提现时需设置支付宝账号姓名
-以上三项可以配置在环境变量
-4.ua_list 抓包的时候把headers里面的user-agent填进来即可，如 ua_list = ['Mozilla/5.0 (Windows NT 10.0; Win64;xxxxxx','Mozilla/5.0 (Windows NT 10.0; Win64;yyyyyy']
+先运行脚本，有问题再到群里问 https://t.me/xiaoymgroup
+new Env('可乐阅读');
 """
 import platform
 import sys
 import os
 import subprocess
-
+import importlib
 
 def check_environment(file_name):
     python_info, os_info, cpu_info = sys.version_info, platform.system().lower(), platform.machine().lower()
     print(
         f"Python版本: {python_info.major}.{python_info.minor}.{python_info.micro}, 操作系统类型: {os_info}, 处理器架构: {cpu_info}")
-    if (python_info.minor in [10]) and os_info in ['linux', 'windows'] and cpu_info in ['x86_64', 'aarch64', 'armv8',
-                                                                                        'amd64']:
-        print("符合运行要求,arm8没试过不知道行不行")
+    if (python_info.minor in [10]) and os_info in ['linux', 'windows'] and cpu_info in ['x86_64', 'aarch64', 'amd64']:
+        print("符合运行要求")
         check_so_file(file_name, os_info, cpu_info)
     else:
         if not (python_info.minor in [10]):
             print("不符合要求: Python版本不是3.10")
         if cpu_info not in ['x86_64', 'aarch64', 'amd64']:
-            print("不符合要求: 处理器架构不是x86_64 aarch64 amd64")
+            print("不符合要求: 处理器架构不是x86_64 aarch64  amd64")
 
 
 def check_so_file(filename, sys_info, cpu_info):
     if sys_info == 'windows':
-        filename = os.path.splitext(filename)[0] + '.pyd'
+        file_name = filename + '.pyd'
     if sys_info == 'linux':
-        filename = os.path.splitext(filename)[0] + '.so'
-    if os.path.exists(filename):
-        print(f"{filename} 存在")
-        import mmyd
-        mmyd.main()
+        file_name = filename + '.so'
+    if os.path.exists(file_name):
+        print(f"{file_name} 存在")
+        try:
+            module = importlib.import_module(filename)
+            module.main()
+        except ImportError:
+            print('文件格式不对，正在删除重新下载')
+            os.remove(file_name)
+            check_so_file(filename, sys_info, cpu_info)
+        except Exception as e:
+            print(e)
     else:
-        print(f"不存在{filename}文件,准备下载文件")
-        url = f'https://gitlab.com/xizhiai/xiaoym/-/raw/master/{os.path.splitext(filename)[0]}'
+        print(f"不存在{file_name}文件,准备下载文件")
+        url = f'https://gitlab.com/xizhiai/xiaoym/-/raw/master/{filename}'
         download_so_file(filename, sys_info, cpu_info, main_url=url)
 
 
@@ -65,16 +62,15 @@ def run_command(command):
 
 
 def download_so_file(filename, sys_info, cpu_info, main_url):
-    file_base_name = os.path.splitext(filename)[0]
     if sys_info == 'windows':
-        url = main_url + f'/{file_base_name}.{cpu_info}_{sys_info}.pyd'
+        file_name = f'{filename}.{cpu_info}_{sys_info}.pyd'
+        f_name = filename +'.pyd'
     if sys_info == 'linux':
-        url = main_url + f'/{file_base_name}.{cpu_info}_{sys_info}.so'
+        file_name = f'{filename}.{cpu_info}_{sys_info}.so'
+        f_name = filename +'.so'
+    url = main_url + '/' + file_name
     print(url)
-    # print(github_url)
-    # 您的命令，使用 -# 参数显示下载进度
-    command = ['curl', '-#', '-o', filename, url]
-    # 执行命令并处理输出
+    command = ['curl', '-#', '-o', f_name, url]
     result = run_command(command)
     if result == 0:
         print(f"下载完成：{filename},调用check_so_file函数")
@@ -84,4 +80,4 @@ def download_so_file(filename, sys_info, cpu_info, main_url):
 
 
 if __name__ == '__main__':
-    check_environment('mmyd.so')
+    check_environment('mmyd')
