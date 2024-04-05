e2c311742e0a13c56b7d3e98462505a6d058ea4c
xizhiai <kafeidou@aliyun.com>
2024-04-05 10:53:55 +0800

    up

--- config_py_to_yaml.py
+++ config_py_to_yaml.py
@@ -21,5 +21,21 @@ conf = {}
 for name in names:
     conf.update({name: getattr(config, name)})
 print(conf)
-with open('1config.yaml', 'w') as fi:
-    yaml.safe_dump(conf, fi, encoding='utf-8', allow_unicode=True)
+lt_config = {
+    'ltck': ['name=德华;auth=Bxxxx','name=彦祖;auth=Bxxxxx'],
+    'max_workers': 3,  # 线程数量设置,设置为5，即最多有5个任务同时进行
+    'shuffle': 0,
+    'txbz': 1000,
+
+    'sendable': 0,  # 企业微信推送开关,1为开，0为关开启后必须设置qwbotkey才能运行
+
+    'pushable': 0,  # wxpusher推送开关,1为开，0为关,开启后必须设置pushconfig才能运行
+
+    'delay_time': 20,  # 并发延迟设置,设置为30即每隔30秒新增一个号做任务，直到数量达到max_workers
+
+    'blacklist': ['name1', 'name2'],  # 提现黑名单设置,黑名单中的账号不自动提现，填入ck中的name,['name1','name2']。
+
+}
+conf.update({'lt_config': lt_config})
+with open('config.yaml', 'w') as fi:
+    yaml.safe_dump(conf, fi, encoding='utf-8')
